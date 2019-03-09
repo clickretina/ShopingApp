863a84a9fdd3e55c5625fe90239350a8f1dc4ff9
clickretina <sanjay.0gnd@gmail.com>
2019-03-09 17:31:42 +0530

    First changes on All in one app some changes in adapterview

--- app/src/main/java/com/example/shopingapp/RecylerviewAdapter.java
+++ app/src/main/java/com/example/shopingapp/RecylerviewAdapter.java
@@ -1,38 +1,60 @@
 package com.example.shopingapp;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class RecylerviewAdapter extends RecyclerView.Adapter<RecylerviewAdapter.ProgrammingViewHolder> {
+    Context applicationcontext;
+    ArrayList<one>arrayList;
+
+
     private String[] data;
-    public RecylerviewAdapter(String[] data)
+    public RecylerviewAdapter(Context context, String[] data)
     {
         this.data=data;
     }
     @Override
     public ProgrammingViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        LayoutInflater inflater=LayoutInflater.from(viewGroup.getContext());
-        View view=inflater(R.layout.activity_main)
-        return new RecylerviewAdapter(view);
-    }
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+           View view=inflater.inflate(R.layout.fragment_one, viewGroup, false);
+           ProgrammingViewHolder programmingViewHolder=new ProgrammingViewHolder(view);
+           return programmingViewHolder;
 
+    }
     @Override
     public void onBindViewHolder(@NonNull ProgrammingViewHolder programmingViewHolder, int positon) {
+
+
+
+
+
+
     }
     private class  MyHolder extends RecyclerView.ViewHolder
     {
+
+        public TextView textView;
+         public ImageView imageView;
         public MyHolder(View itemView){
             super(itemView);
+            textView.findViewById(R.id.textView);
+            imageView.findViewById(R.id.imageView);
         }
 
     }
 
     @Override
-    public int getItemCount() {
-        return data.length;
+    public int getItemCount()
+    {
+        return arrayList.size();
     }
 
     public  class ProgrammingViewHolder extends RecyclerView.ViewHolder{
--- app/src/main/java/com/example/shopingapp/one.java
+++ app/src/main/java/com/example/shopingapp/one.java
@@ -3,6 +3,8 @@ package com.example.shopingapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.example.shopingapp.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,9 +30,18 @@ public class one extends Fragment {
         View view = inflater.inflate(R.layout.fragment_one, container, false);
         final String[] menuItem = {"What is Andriod ?", "Andriod History", "Why Andriod ?", "Andriod Software Stack", "Dalvik Virtual Machine", "Art vs DAlvik"
                 ,"Andriod Development Components"};
+        ArrayList<one>arrayList =new ArrayList<>();
+
+
         recyclerView = (RecyclerView) view.findViewById(R.id.rv);
-        RecylerviewAdapter.setAdapter(new RecylerviewAdapter(menuItem));
-        RecylerviewAdapter.ProgrammingViewHolder(new(getContext(),menuItem));
+        RecylerviewAdapter myadapter=new RecylerviewAdapter(getContext(),menuItem);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getContext()) ;
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator() );
+
+
+              recyclerView.setAdapter(myadapter);
+
 
         return view;
     }
